890097b71b2e7357cc2dd9161fb8fbb8f396e6cc
Taj K <taj.kirkpatrick@gmail.com>
2022-05-19 17:21:52 -0500

    start NavBar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import turbolinks from '@astrojs/turbolinks';
+import vue from '@astrojs/vue';
 import { defineConfig } from 'astro/config';
-
-import vue from "@astrojs/vue";
-import tailwind from "@astrojs/tailwind";
-import turbolinks from "@astrojs/turbolinks";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind(), turbolinks(), sitemap()]
+  integrations: [vue(), tailwind(), turbolinks(), sitemap()],
+  site: 'https://www.tajkirkpatrick.com/',
+  vite: {
+    build: {
+      emptyOutDir: true,
+    },
+    server: {
+      host: '127.0.0.1',
+    },
+  },
 });
