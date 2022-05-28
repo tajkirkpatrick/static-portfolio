d9120a4115c9604ceaf57c1e39ac3f3a0b5c2a51
Taj K <taj.kirkpatrick@gmail.com>
2022-05-27 21:39:25 -0500

    Succesfully Registering PWA Manifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import turbolinks from '@astrojs/turbolinks';
 import vue from '@astrojs/vue';
 import { defineConfig } from 'astro/config';
-// import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +16,38 @@ export default defineConfig({
     server: {
       host: '127.0.0.1',
     },
-    // plugins: [VitePWA()],
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+        manifest: {
+          id: '8f89565b-6081-4b95-989a-ed4902eaab0d',
+          name: 'Taj Kirkpatrick Web Services',
+          short_name: 'TKWS',
+          description: 'PWA powered by Astro JS',
+          theme_color: '#ffffff',
+          background_color: '#ffffff',
+          start_url: '/',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
+    ],
   },
 });
