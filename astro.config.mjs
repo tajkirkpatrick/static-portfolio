24f9caa1701e11ce8069e3de45ca59864442ae59
Taj K <taj.kirkpatrick@gmail.com>
2022-05-28 00:43:38 -0500

    Remove Vue Dev Dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import turbolinks from '@astrojs/turbolinks';
-import vue from '@astrojs/vue';
 import { defineConfig } from 'astro/config';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind(), turbolinks(), sitemap()],
+  integrations: [tailwind(), turbolinks(), sitemap()],
   site: 'https://www.tajkirkpatrick.com/',
   vite: {
     build: {
